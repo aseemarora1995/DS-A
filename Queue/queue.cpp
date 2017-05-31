fd94be883a2f926b1ff76a6543363c8696cd9a94
aseem arora <aseemarora1995@gmail.com>
2017-05-31 17:24:26 +0530

    implement queue usind vector

---
+++ Queue/queue.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define MAX_SIZE 10
+
+class Queue{
+private:
+    vector<int> v = vector<int>(MAX_SIZE);
+    int frontt = -1, rear = -1;
+
+public:
+    void enqueue(int);
+    void dequeue();
+    bool isFull();
+    bool isEmpty();
+    void peek();
+    void print();
+};
+void Queue::enqueue(int x){
+    if(isFull()){
+        cout << "Queue's full. Overflow.\n";
+        return;
+    }
+    if(isEmpty()){
+        frontt = rear = 0;
+        v[rear] = x;
+    }
+    else{
+        v[++rear] = x;
+    }
+}
+void Queue::dequeue(){
+    if(isEmpty()){
+        cout << "Queue's empty. Undeflow.\n";
+        return;
+    }
+    if(frontt == rear)
+        frontt = rear = -1;
+    else
+        frontt++;
+}
+void Queue::peek(){
+    if(isEmpty()){
+        cout << "Underflow\n";
+        return;
+    }
+    cout << "Front element: " << v[frontt] << endl;
+}
+bool Queue::isEmpty(){
+    if(frontt==-1 && rear==-1)
+        return true;
+    return false;
+}
+bool Queue::isFull(){
+    if(rear == MAX_SIZE-1)
+        return true;
+    return false;
+}
+void Queue::print(){
+    if(isEmpty()){
+        cout << "Queue's empty.\n";
+        return;
+    }
+    cout << "Queue: ";
+    for(vector<int>::iterator it=v.begin()+rear; it>v.begin()+frontt-1; it--){
+        cout << *it << " ";
+    }
+    cout << endl << "rear: " << rear << "   front: " << frontt << endl;
+}
+
+int main(){
+    Queue q;
+    int choice, value;
+    char yes;
+
+    while(true){
+        cout << "\n1: enqueue\n2: dequeue\n3: peek\n4: isEmpty\n5: print\nenter choice: ";
+        cin >> choice;
+        switch(choice){
+            case 1:
+                cout << "enter value to push: ";
+                cin >> value;
+                q.enqueue(value); q.print();
+                break;
+
+            case 2:
+                q.dequeue();q.print();
+                break;
+
+            case 3:
+                q.peek();q.print();
+                break;
+
+            case 4:
+                if(q.isEmpty()){
+                    cout << "Queue's empty.\n";
+                }
+                else{
+                    cout << "Queue's not empty\n";
+                }
+                break;
+
+            case 5:
+                q.print();
+        }
+    }
+    return 0;
+}
