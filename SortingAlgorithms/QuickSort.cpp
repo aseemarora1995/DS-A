61040e87473f973edbff3a6b90948adc46adcd7e
aseem arora <aseemarora1995@gmail.com>
2017-06-05 15:55:54 +0530

    implements quick sort. improved upon the previous code.

--- SortingAlgorithms/QuickSort.cpp
+++ SortingAlgorithms/QuickSort.cpp
@@ -4,25 +4,29 @@ using namespace std;
 
 typedef vector<int> ivec;
 
+void print(ivec &);
+
 int Partition(ivec &v, int start, int endd){
     int pivot = start;
+    int next = pivot + 1;
     cout << "pivot: " << pivot << endl;
-    for(int i=start; i<endd; i++){
+    for(int i=start+1; i<endd; i++){
         if(v[pivot] > v[i]){
-            int temp = v[i];
-            for(int j=i; j>pivot-1; j--){
-                v[j] = v[j-1];
-            }
-            v[pivot] = temp;
-            pivot++;
+            if(next < i)
+                swap(v[next], v[i]);
+            next++;
         }
     }
+    swap(v[pivot], v[next-1]);
+    pivot = next-1;
     cout << "pivot: " << pivot << endl;
+    print(v);
     return pivot;
+
 }
 
 void QuickSort(ivec &v, int start, int endd){
-    cout << "Quicksort: " << start << "  " << endd << endl;
+    // cout << "Quicksort: " << start << "  " << endd << endl;
     int vec_size = endd-start;
     if(vec_size <= 1)
         return;
@@ -40,7 +44,7 @@ void print(ivec &v){
 
 int main(){
     int n, start = 0, pivot;
-    cout << "size:"; cin >> n;
+    cout << "size:"; cin >> n; cout << "array: ";
     ivec v(n);
     for(int i=0; i<n; i++){
         cin >> v[i];
